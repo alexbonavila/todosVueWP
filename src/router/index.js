4eec3f1a39604aa1e01ad4d9ad6d768eceaa5932
alex <alexbonavila44@gmail.com>
2017-06-02 14:34:21 +0200

    Middleware auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,89 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import auth from '../services/auth'
 import Hello from '@/components/Hello'
 import Login from '@/components/Login'
 
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    }
-  ]
+
+const routes = [
+  { path: '/android_asset/www/index.html',
+    component: Login,
+    meta: { auth: false }
+  },
+  {
+    path: '/',
+    component: Hello,
+    meta: { auth: true }
+  },
+  // {
+  //   path: '/todos',
+  //   component: Todos,
+  //   meta: { auth: true }
+  // },
+  // {
+  //   path: '/tokens',
+  //   component: Tokens,
+  //   meta: { auth: false }
+  // },
+  // {
+  //   path: '/profile',
+  //   component: Profile,
+  //   meta: { auth: true }
+  // },
+  // {
+  //   path: '/device_info',
+  //   component: DeviceInfo,
+  //   meta: { auth: false }
+  // },
+  // { path: '/cordova',
+  //   component: Cordova,
+  //   meta: { auth: false }
+  // },
+  {
+    path: '/login',
+    component: Login,
+    meta: { auth: false }
+  },
+  // { path: '/notifications',
+  //   component: Notifications,
+  //   // props: Notifications.get(),
+  //   meta: { auth: true }
+  // },
+  // {
+  //   path: '*',
+  //   component: NotFound,
+  //   meta: { auth: false }
+  // }
+]
+
+const router = new Router({
+  mode: 'history',
+  routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !auth.loggedIn()) {
+    return next('/login')
+  }
+  next()
+})
+
+export default router
+
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'Hello',
+//       component: Hello
+//     },
+//     {
+//       path: '/login',
+//       name: 'Login',
+//       component: Login
+//     }
+//   ]
+// })
